b6ef612fdeee2dd5b39a67dd8ac71dcdf0640fef
Bruno Meireles <bruno.meirelessilva90@gmail.com>
2026-07-16 11:00:17 -0300

    feat: start header implementation

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+function Header() {
+    return (
+        <header>
+            <h1>Mega Store</h1>
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a>Home</li>
+                    <li><a href="#">Products</a>Products</li>
+                    <li><a href="#">About</a>Sobre</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header;
