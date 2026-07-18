d7418dd89ce0a85113b6f916530f72be7184844c
Bruno Meireles <bruno.meirelessilva90@gmail.com>
2026-07-18 09:19:36 -0300

    feat: implementa cabeçalho com navegação

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,22 @@
+import "./Header.css";
+
 function Header() {
-    return (
-        <header>
-            <h1>Mega Store</h1>
-            <nav>
-                <ul>
-                    <li><a href="#">Home</a>Home</li>
-                    <li><a href="#">Products</a>Products</li>
-                    <li><a href="#">About</a>Sobre</li>
-                </ul>
-            </nav>
-        </header>
-    )
+  return (
+    <header className="header">
+      <div className="container header__inner">
+        <a href="#inicio" className="header__logo">
+          Mega<span>Store</span>
+        </a>
+        <nav className="header__nav">
+          <ul>
+            <li><a href="#inicio">Início</a></li>
+            <li><a href="#produtos">Produtos</a></li>
+            <li><a href="#contato">Contato</a></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
